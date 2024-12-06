623f1d3f1b6641b6e7ec9efb7e4d100002de8cf1
Tiago Azevedo <up202108699@edu.fe.up.pt>
2024-12-06 04:20:39 +0000

    add routes to pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,41 @@
 import React, { useState } from "react";  
-import MainContent from './components/MainContent';
-import Sidebar from './components/Sidebar';
-import Header from "./components/Header";
+//import MainContent from './components/MainContent';
+//import Sidebar from './components/Sidebar';
+//import Header from "./components/Header";
+//
+//function App() {
+//    const [selectedProject, setSelectedProject] = useState(null);
+//    const [isOpen, setIsOpen] = useState(true);
+//  return (
+//    <>
+//        <Header setSideBarOpen={setIsOpen} isSideBarOpen={isOpen} />
+//          <div id="main">
+//              <Sidebar setSelectedProject={setSelectedProject} isOpen={isOpen}/>
+//              <MainContent selectedProject={selectedProject} setSelectedProject={setSelectedProject} />
+//          </div>
+//    </>
+//  );
+//}
 
-function App() {
-    const [selectedProject, setSelectedProject] = useState(null);
-    const [isOpen, setIsOpen] = useState(true);
+// App.js
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import InputSection from './components/InputSection';
+import ProjectPage from './components/ProjectPage';
+import Layout from "./components/Layout";
+
+const App = () => {
+  const [update, setUpdate] = useState(false);
   return (
-    <>
-        <Header setSideBarOpen={setIsOpen} isSideBarOpen={isOpen} />
-          <div id="main">
-              <Sidebar setSelectedProject={setSelectedProject} isOpen={isOpen}/>
-              <MainContent selectedProject={selectedProject} setSelectedProject={setSelectedProject} />
-          </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout update = {update} setUpdate = {setUpdate}/>}>
+          <Route index element={<InputSection  setUpdate = {setUpdate}/>} />
+          <Route path="project/:projectId" element={<ProjectPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
